c69c9a3c6ce944a5a59d7e94df8a60ac576a89eb
Ronelle Cudjoe <ronnycudjoe9@gmail.com>
2025-03-06 21:58:51 +0000

    created news service component to handle newsapi calls

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,15 +2,39 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { SupabaseService } from '../supabase.service';
+import { FormsModule } from '@angular/forms';
+import { SupabaseClient } from '@supabase/supabase-js';
 
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [MatCardModule, MatInputModule, MatButtonModule],
+  imports: [MatCardModule, MatInputModule, MatButtonModule, FormsModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
-export class AuthComponent {
+export class AuthComponent { 
+  email: string = '';
+  password: string = '';
 
+  constructor (private supabase: SupabaseService) {};
+
+  async onSignUp() {
+    const { data, error } = await this.supabase.signUp(this.email, this.password);
+    if (error) {
+      console.error('Signup error:', error.message);
+    } else {
+      console.log('Signed up as:', data.user);
+    }
+  }
+
+  async onLogin() {
+    const { data, error } = await this.supabase.signIn(this.email, this.password);
+    if (error) {
+      console.error('Login error:', error.message);
+    } else {
+      console.log('Logged in as:', data.user);
+    }
+  }
 }
