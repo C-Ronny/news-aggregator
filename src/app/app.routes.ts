ff57c9f3f50c6a0b8a28e89136f68a6ebfae04fd
Ronelle Cudjoe <ronnycudjoe9@gmail.com>
2025-03-06 13:37:00 +0000

    created routes to home, login and signup pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Creates routes for hom and authentication navigation
+import { HomeComponent } from  './home/home.component';
+import { AuthComponent } from  './auth/auth.component';
+
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent, title: 'Home'},
+  { path: 'login', component: AuthComponent, title: 'Login'},
+  { path: 'signup', component: AuthComponent, title: 'Signup'},
+  { path: '**', redirectTo: ''} // Redirect to Home for unknown routes
+
+];
