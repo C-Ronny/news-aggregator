ba06f009ebe2fbf24dca28415ee53a6e94b085b7
Ronelle Cudjoe <ronnycudjoe9@gmail.com>
2025-03-08 14:13:17 +0000

    created saved articles page and link to it

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 // Creates routes for hom and authentication navigation
 import { HomeComponent } from  './home/home.component';
 import { AuthComponent } from  './auth/auth.component';
+import { SavedArticlesComponent } from './saved-articles/saved-articles.component';
 
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, title: 'Home'},
+  { path: 'saved', component: SavedArticlesComponent, title: 'Saved Articles'},
   { path: 'login', component: AuthComponent, title: 'Login'},
   { path: 'signup', component: AuthComponent, title: 'Signup'},
   { path: '**', redirectTo: ''} // Redirect to Home for unknown routes
